e44fbf806e4fada590f6780d6f89496898148fd8
Sachin Kumar Thakur <sachinkumarthakur878@gmail.com>
2026-03-01 08:12:27 +0530

    frontend intigration login and signup

---
+++ Backend/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cookieparser = require('cookie-parser')
+const cors = require('cors')
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors({ 
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
+
+const authRouter = require("./routes/auth.routes")
+const postRouter = require("./routes/post.routes")
+const userRouter = require("./routes/user.routes")
+
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+
+
+module.exports = app;
